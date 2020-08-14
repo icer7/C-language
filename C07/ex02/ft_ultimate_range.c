c5036e24c395592f948bafd4f7e0a68caadfd99b
icer7 <ryo0104777@gmail.com>
2020-08-14 17:35:01 +0900

    [Add]C07

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rysato <rysato@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/14 16:44:17 by rysato            #+#    #+#             */
+/*   Updated: 2020/08/14 16:44:19 by rysato           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_ultimate_range(int **range, int min, int max)
+{
+	int	bound;
+	int	index;
+	int	*buffer;
+
+	if (min >= max)
+	{
+		*range = 0;
+		return (0);
+	}
+	bound = max - min - 1;
+	if ((buffer = malloc(bound * sizeof(int))) == NULL)
+	{
+		*range = 0;
+		return (-1);
+	}
+	*range = buffer;
+	index = 0;
+	while (index <= bound)
+	{
+		buffer[index] = min + index;
+		index++;
+	}
+	return (bound + 1);
+}
